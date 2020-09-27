7c14e015ca88106394b66b86cc4154e7aa63c41b
Borrison <borissoto@gmail.com>
2020-09-26 21:30:01 -0400

    little test

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,7 +8,7 @@ class SearchBar extends React.Component {
       <div className='ui segment'>
         <form className='ui form'>
           <div className='field'>
-            <label>Image Search</label>
+            <label>Image Search!</label>
             <input
               type='text'
               value={this.state.term}
