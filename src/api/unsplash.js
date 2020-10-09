b1630b57eb12d871f7f5e1dfdf6016759c18710c
Borrison <borissoto@gmail.com>
2020-10-09 00:28:56 -0400

    showing images

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -5,6 +5,6 @@ const API_ID = process.env.REACT_APP_UNSPLASH_API_ID;
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: '',
+    Authorization: 'Client-ID ' + API_ID,
   },
 });
