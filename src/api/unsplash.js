576bf77a77229999282574a6b0e6b34f741060a4
Borrison <borissoto@gmail.com>
2020-10-08 23:53:56 -0400

    adding imagelist component

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const API_ID = process.env.REACT_APP_UNSPLASH_API_ID;
+
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: 'Client-ID',
+    Authorization: '',
   },
 });
