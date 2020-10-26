1023f75fb854e22c15f8fa037b67ecadc59084da
Borrison <borissoto@gmail.com>
2020-10-25 23:24:56 -0400

    Ref system for reach into the DOM

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
 import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard'
 
 const ImageList = (props) => {
-  const images = props.images.map(({ alt_description, id, urls }) => {
+  const images = props.images.map(( image ) => {
     //   assign an Id to the root element
-    return <img key={id} src={urls.regular} alt={alt_description} />;
+    return <ImageCard key={image.id} image={image} />;
   });
 
   return <div className="image-list">{images}</div>;
